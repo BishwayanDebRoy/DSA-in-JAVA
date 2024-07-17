618eff348dff71dd7102ae7b6b703aad0c67d567
BishwayanDebRoy <bb3546@srmist.edu.in>
2024-07-17 13:31:57 +0530

    basic file is created

---
+++ Basics/JavaBasics.java
@@ -0,0 +1,6 @@
+public class JavaBasics {
+    public static void main(String args[]) {
+        System.err.println("hello");
+    }
+}
+// the compiler searches for the main function for execution of the code.
