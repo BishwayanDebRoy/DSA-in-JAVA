3f889c07c53cbaed13da04c3d7641451abd6df67
BishwayanDebRoy <bb3546@srmist.edu.in>
2024-07-17 13:44:46 +0530

    Simple pattern with print statement is printed

--- Basics/JavaBasics.java
+++ Basics/JavaBasics.java
@@ -1,6 +1,11 @@
+package Basics;
+
 public class JavaBasics {
     public static void main(String args[]) {
         System.err.println("hello");
     }
-}
+};
+// boilerplate code:the code written every time whenever we want to write the
+// code in java
+
 // the compiler searches for the main function for execution of the code.
---
+++ Basics/SimplePattern.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class SimplePattern {
+    public static void main(String args[]){
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        
+    }
+}
