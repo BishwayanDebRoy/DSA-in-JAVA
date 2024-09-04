ef230337b034cfe07573ffeb24141ef717ade162
BishwayanDebRoy <bb3546@srmist.edu.in>
2024-09-04 14:25:46 +0530

    Armstrong no. is learnt

---
+++ BasicMaths/ArmstrongNo.java
@@ -0,0 +1,30 @@
+//An armstrong no. is a no. that is equal to the sum of the cube of each digit of the number
+//EG: for 371 , by adding the digits we get: 3^3+7^3+1^3=371
+
+//which is same as the original no. so thus it is an armstront no.
+
+package BasicMaths;
+
+import java.util.*;
+
+public class ArmstrongNo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int orgnum = num;
+        int armstrongno = 0;
+        while (num != 0) {
+            int lastdigit = num % 10;
+            armstrongno += (lastdigit * lastdigit * lastdigit);
+            num /= 10;
+
+        }
+        if (orgnum == armstrongno) {
+
+            System.out.println(orgnum + "is an Armstrong No.");
+        } else {
+            System.out.println(orgnum + "is not an Armstrong No.");
+        }
+        sc.close();
+    }
+}
