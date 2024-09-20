eb819ccad6f25cb0b404a253964bdd7c105eda24
BishwayanDebRoy <bb3546@srmist.edu.in>
2024-09-21 00:50:57 +0530

    large small sum is added

---
+++ Accenture/LargeSmallSum.java
@@ -0,0 +1,12 @@
+package Accenture;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LargeSmallSum {
+
+    public static void main(String[] args) {
+        int arr[] = { 3, 2, 1, 7, 5, 4 };
+        System.out.println(arr);
+    }
+}
