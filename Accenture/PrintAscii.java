bbddc5a64a504f77e8e28dcbaf137f93e3b7eed2
BishwayanDebRoy <bb3546@srmist.edu.in>
2024-09-24 17:12:03 +0530

    code is modified for ascii value

--- Accenture/PrintAscii.java
+++ Accenture/PrintAscii.java
@@ -9,9 +9,10 @@ public class PrintAscii {
         String rev = "";
         int ascii = (int) str.charAt(0);
 
-        for (int i = 0; i < str.length(); i++) {
-            rev = str.charAt(i) + rev;
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
         }
+        System.out.println("Reversed string is:" + rev);
         if (rev.equals(str)) {
             System.out.println(str.length());
         } else {
